bcadcf5a33c69066ca8a209b258241eb5a4f3214
nnhanh <n.hanhdr@gmail.com>
2023-12-16 21:44:17 +0700

    fix

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh                                                                                                 
 
-sudo cp /home/orangepi/update/microservices.usbport-1.0.0 /usr/local/bin/microservices.usbport-1.0.0 
+sudo cp /home/orangepi/update_usb/microservices.usbport-1.0.0 /usr/local/bin/microservices.usbport-1.0.0
 
 history -c
