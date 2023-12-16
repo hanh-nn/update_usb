56301dda9c1409711a3935eaaa9ad356972c7534
nnhanh <n.hanhdr@gmail.com>
2023-12-16 22:54:06 +0700

    .

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ sudo systemctl stop microservices.usbport.service
 sudo cp /home/orangepi/update_usb/microservices.usbport-1.0.0 /usr/local/bin/microservices.usbport-1.0.0                                                 
 echo 'update usb success v.1.1' | tee /home/orangepi/ver_note.txt
 rm ~/.bash_history
-rm -r update_usb
+sudo rm -r update_usb
 sudo reboot
