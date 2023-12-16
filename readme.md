408b3404988c48599a9a4e66ec4dc794f2ed5fcf
nnhanh <n.hanhdr@gmail.com>
2023-12-16 22:45:37 +0700

    up

---
+++ readme.md
@@ -0,0 +1,2 @@
+git clone https://github.com/hanh-nn/update_usb.git
+scp orangepi@192.168.5.13:/home/orangepi/microservices/bin/microservices.usbport-1.0.0 .
--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
-#!/bin/sh                                                                                                 
-
-sudo cp /home/orangepi/update_usb/microservices.usbport-1.0.0 /usr/local/bin/microservices.usbport-1.0.0
-
-history -c
+#!/bin/sh
+sudo systemctl stop microservices.usbport.service
+sudo cp /home/orangepi/update_usb/microservices.usbport-1.0.0 /usr/local/bin/microservices.usbport-1.0.0                                                 
+echo 'update usb success v.1.1' | tee /home/orangepi/ver_note.txt
+rm ~/.bash_history
+rm update_usb
+sudo reboot
